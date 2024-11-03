dc9d0c68f36a9e677d589acf825b8ff608fd8300
isard <jborras89@gmail.com>
2024-11-03 22:47:04 +0100

    segon commit

--- eac2Exercici2/settings.py
+++ eac2Exercici2/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-(^be=umqdh%d5kv3pl9t+&!hl(z!4y%an_f8(zg=9)12igba60
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
